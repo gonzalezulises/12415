ec68f7352faf92ea73160c0114dd002f87ee71ec
Ulises Gonzalez <gonzalez.ulises@gmail.com>
2021-01-25 20:46:54 -0500

    actualizacion de logica

--- algoritmo.js
+++ algoritmo.js
@@ -1,16 +1,37 @@
 var diausuario=prompt("ingrese un dia de la semana");
 var diassemana= ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+if (diausuario == "lunes"){
+    a=1;
+}
+else if (diausuario == "martes"){
+    a=2;
+}
+else if (diausuario == "miercoles"){
+    a=3;
+}
+else if (diausuario == "jueves"){
+    a=4;
+}
+else if (diausuario == "viernes"){
+    a=5;
+}
+else if (diausuario == "sabado"){
+    a=6;
+}
+else if (diausuario == "domingo"){
+    a=7;
+}
 
 
 for (var i=0 ;i<7;i++) {
-  if (diassemana[i] = diausuario ){
+  if (diassemana[i] = a ){
  diapar ()
   break;
  } 
 }
 
 function diapar(i){
-    if(i = 2 ||4||6)  {
+    if(a = 2||4||6)  {
   alert("EL dia de la semana es par")
   }
 else {
