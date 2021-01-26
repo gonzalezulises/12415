c69ad3c22531a88cefd4efe1a60c092e291c43b7
Ulises Gonzalez <gonzalez.ulises@gmail.com>
2021-01-25 19:53:04 -0500

    actualizacion del ejercicio

---
+++ algoritmo.js
@@ -0,0 +1,19 @@
+var diausuario=prompt("ingrese un dia de la semana");
+var diassemana= ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+
+for (var i=1 ;i<7;i++) {
+  if (diassemana[i] = diausuario ){
+ diapar ()
+  break;
+ } 
+}
+
+function diapar(i){
+    if(i = 2 ||4||6)  {
+  alert("EL dia de la semana es par")
+  }
+else {
+  alert("El dia es impar");
+  }
+}  
