fed292656579e38c7db91974139e071eae768ba0
Ulises Gonzalez <gonzalez.ulises@gmail.com>
2021-02-01 16:25:13 -0500

    Desafio 5 sobre Objetos

---
+++ algoritmod5.js
@@ -0,0 +1,15 @@
+function inventario (marca,tipo) {
+    this.marca = marca
+    this.tipo = tipo;
+  };
+
+  let getDatosPCmarca = prompt ("¿Qué marca de computadora deseas ingresar","Dell,Hp,Alienware,MSI");
+  let getDatosPCtipo = prompt ("¿Qué tipo de computadora deseas ingresar","desktop,notebook");    
+  
+
+var getDatos = new inventario ( getDatosPCmarca,getDatosPCtipo)
+
+console.log (getDatos)
+
+
+
